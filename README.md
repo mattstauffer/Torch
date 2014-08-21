082ec03dd8363adfc9d34dd03766c66123402749
Matt Stauffer <mstauffer@gmail.com>
2014-08-21 19:01:49 -0400

    Add vendor to gitignore; add Eloquent.

--- README.md
+++ README.md
@@ -2,13 +2,21 @@
 
 Using a naked Slim project to test out bootstraps for Illuminate packages.
 
+## Usage
+At the moment, the project is divided into many directories beneath public which will each contain a Slim index file. Navigate to that directory in your terminal and run the following to serve a web site from that directory:
+
+```bash
+php -S localhost:8000
+```
+
+Now you can visit http://localhost:8000/ in your browser to view the output of each.
+
 ## Packages
 
-### Done
- 
+### Started or Done
+ * [Eloquent](https://github.com/mattstauffer/IlluminateSlim/tree/master/public/eloquent)
 
 ### Planned
- * Eloquent
  * Pagination
  * IOC
  * Cache
---
+++ public/eloquent/index.php
@@ -0,0 +1,71 @@
+<?php
+
+require '../../vendor/autoload.php';
+
+use App\Eloquent\User;
+use App\Eloquent\UserEncapsulated;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+
+/**
+ * Illuminate/database, aka Eloquent, can be used via Capsule. See below a plain MVP,
+ * and also a class-encapsulated option
+ *
+ * Requires: illuminate/database
+ *
+ * @source https://github.com/illuminate/database
+ */
+
+$app = new \Slim\Slim();
+$app->add(new \Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware);
+
+$app->get('/', function () {
+
+	$capsule = new Capsule;
+
+	$capsule->addConnection([
+		'driver'    => 'mysql',
+		'host'      => 'localhost',
+		'database'  => 'illuminateslim',
+		'username'  => 'root',
+		'password'  => '',
+		'charset'   => 'utf8',
+		'collation' => 'utf8_unicode_ci',
+		'prefix'    => '',
+	]);
+
+	// Set the event dispatcher used by Eloquent models... (optional)
+	$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+	// Set the cache manager instance used by connections... (optional)
+	// $capsule->setCacheManager(...);
+
+	// Make this Capsule instance available globally via static methods... (optional)
+	$capsule->setAsGlobal();
+
+	// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+	$capsule->bootEloquent();
+
+	// Use it
+	echo '<pre>';
+
+	$user = Capsule::table('users')->where('id', '=', 1)->get();
+
+	var_dump($user);
+
+	$users = User::all();
+
+	var_dump($users);
+
+	// More examples and docs here: https://github.com/illuminate/database
+});
+
+$app->get('/encapsulated', function() {
+	echo '<pre>';
+
+	$users = UserEncapsulated::all();
+
+	var_dump($users);
+});
+$app->run();
