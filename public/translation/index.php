bc59b3c12b457a918b652c55d62bc3e587ba0aba
Robin Malfait <malfait.robin@gmail.com>
2014-08-29 13:29:43 +0200

    Adding awesome translations

--- public/translation/index.php
+++ public/translation/index.php
@@ -2,18 +2,35 @@
 
 /*
 |--------------------------------------------------------------------------
-| Composer:
+| Use me right?
 |--------------------------------------------------------------------------
 */
+
+use Illuminate\Translation\FileLoader;
+use Illuminate\Translation\Translator;
+use Illuminate\Filesystem\Filesystem;
+use Slim\Slim;
+use Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware;
+
+
+/*
+|--------------------------------------------------------------------------
+| Composer: "illuminate/translation": "4.2.8"
+|--------------------------------------------------------------------------
+*/
+
 require_once '../../vendor/autoload.php';
+
 
 /*
 |--------------------------------------------------------------------------
 | Instantiate it
 |--------------------------------------------------------------------------
 */
-$app = new \Slim\Slim();
-$app->add(new \Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware);
+
+$app = new Slim();
+$app->add(new WhoopsMiddleware);
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,9 +38,28 @@ $app->add(new \Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware);
 |--------------------------------------------------------------------------
 */
 
+$app->get('/', function()
+{
+    /* Getting the file loader ready */
+    $loader = new FileLoader(new Filesystem(), './lang');
+
+    /* English translator (You can change the "en" to a getLocale function) */
+    $transEnglish = new Translator($loader, "en");
+
+    /* Dutch translator */
+    $transDutch = new Translator($loader, "nl");
+
+    echo "<h1>Translations right?!</h1><pre>";
+
+    echo "English: " . $transEnglish->get('talk.conclusion') . "\n";
+    echo "Dutch:   " . $transDutch->get('talk.conclusion');
+});
+
+
 /*
 |--------------------------------------------------------------------------
 | Boot it
 |--------------------------------------------------------------------------
 */
+
 $app->run();
