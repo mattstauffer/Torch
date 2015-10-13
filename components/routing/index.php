77ab67e3ed2cafc036c90d7c6e16b34f1ebb4f35
Jason Walton <jwalton512@gmail.com>
2015-10-12 20:19:59 -0700

    add routing example

---
+++ components/routing/index.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Illuminate/Routing
+ *
+ * @source https://github.com/illuminate/routing
+ * @author https://github.com/jwalton512
+ */
+
+require_once 'vendor/autoload.php';
+
+use Illuminate\Container\Container;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Router;
+
+// Using Illuminate/Events/Dispatcher here (not required),
+// any implementation of Illuminate/Contracts/Event/Dispatcher
+// is acceptable
+$events = new Dispatcher(new Container);
+
+// Create the router instance
+$router = new Router($events);
+
+// Load the routes
+require_once 'routes.php';
+
+// Create a request from server variables
+$request = Request::capture();
+
+// Dispatch the request through the router
+$response = $router->dispatch($request);
+
+// Send the response back to the browser
+$response->send();
