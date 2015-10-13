a1eafe44ebac38bd5365aea4a602546c2b6a8841
Dejan Angelov <angelovdejan92@gmail.com>
2015-10-13 12:22:16 +0200

    dispatching the requests

---
+++ components/routing/index.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Events\Dispatcher;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Router;
+
+include 'vendor/autoload.php';
+
+$router = new Router(new Dispatcher());
+
+$request = Request::createFromGlobals();
+
+$response = $router->dispatch($request);
+
+$response->send();
