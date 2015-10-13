82de311c2ef5f98dcd40b65313c69047dfafa5c1
Matt Stauffer <matt@tighten.co>
2015-10-13 13:25:56 -0400

    Move cache to component directory, update cache index.php for clarity

--- components/cache/index.php
+++ components/cache/index.php
@@ -17,27 +17,32 @@ $app->add(new \Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware);
 
 $app->get('/', function () {
     // Create a new Container object, needed by the cache manager.
-    $container           = new Container;
+    $container = new Container;
+
     // The CacheManager creates the cache "repository" based on config values
     // which are loaded from the config class in the container.
-    // More about the config class can be found in the config component, for now we will use an array
+    // More about the config class can be found in the config component; for now we will use an array
     $container['config'] = [
-        'cache.default'     => 'file',
+        'cache.default' => 'file',
         'cache.stores.file' => [
             'driver' => 'file',
-            'path'   => __DIR__ . '/../../cache'
+            'path' => __DIR__ . '/cache'
         ]
     ];
+
     // To use the file cache driver we need an instance of Illuminate's Filesystem, also stored in the container
-    $container['files']  = new Filesystem;
+    $container['files'] = new Filesystem;
+
     // Create the CacheManager
     $cacheManager = new CacheManager($container);
+
     // Get the default cache driver (file in this case)
-    $cache        = $cacheManager->store();
-    // The following would work as well, especially if you have multiple drivers configured
-    //$cache        = $cacheManager->store('file');
-    // Store or "put" a value into cache for 500 minutes
-    // You can find the cache files within the root directory
+    $cache = $cacheManager->store();
+
+    // Or, if you have multiple drivers:
+    // $cache = $cacheManager->store('file');
+
+    // Store a value into cache for 500 minutes
     $cache->put('test', 'This is loaded from cache.', 500);
 
     // Echo out the value we just stored in cache
