6ae0e9eae385b203eec922cbfcd68ad099e15419
Dejan Angelov <angelovdejan92@gmail.com>
2015-10-15 14:12:42 +0200

    added examples with grouping and controller

---
+++ components/routing/src/Controllers/UsersController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class UsersController
+{
+    public function index()
+    {
+        return "listing the users";
+    }
+
+    public function store()
+    {
+        return "creating new user";
+    }
+}
