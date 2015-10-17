57f80b90ce6b675c3ebd9a59178fdc6c9ef8d142
Hunter Skrasek <hunterskrasek@me.com>
2015-10-17 13:44:06 -0500

    Remove the HTTP Kernel and ExceptionHandler bindings
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- components/artisan/bootstrap/app.php
+++ components/artisan/bootstrap/app.php
@@ -27,18 +27,8 @@ $app = new Illuminate\Foundation\Application(
 */
 
 $app->singleton(
-    Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
-);
-
-$app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
     App\Console\Kernel::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
 );
 
 /*
