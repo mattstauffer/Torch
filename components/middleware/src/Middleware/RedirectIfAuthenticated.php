ca0609ef37f3e69931f8dc20751c2d3c3c0c6ccf
Иван Горохов <cwteam@bk.ru>
2016-09-15 21:33:09 +0700

    Middleware

---
+++ components/middleware/src/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if ($_SESSION['user']) {
+            return 'Error Authenticate';
+        }
+
+        return $next($request);
+    }
+}
