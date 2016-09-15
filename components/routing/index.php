a451a83158b2c66e3a3ec964f5f2d3261009d881
Иван Горохов <cwteam@bk.ru>
2016-09-15 21:38:59 +0700

    Redirect

--- components/routing/index.php
+++ components/routing/index.php
@@ -7,6 +7,7 @@
  * @contributor Muhammed Gufran
  * @contributor Matt Stauffer
  * @contributor https://github.com/jwalton512
+ * @contributor https://github.com/dead23angel
  */
 
 require_once 'vendor/autoload.php';
@@ -14,7 +15,9 @@ require_once 'vendor/autoload.php';
 use Illuminate\Container\Container;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 use Illuminate\Routing\Router;
+use Illuminate\Routing\UrlGenerator;
 
 // Using Illuminate/Events/Dispatcher here (not required); any implementation of
 // Illuminate/Contracts/Event/Dispatcher is acceptable
@@ -28,6 +31,14 @@ require_once 'routes.php';
 
 // Create a request from server variables
 $request = Request::capture();
+
+// Create the redirect instance
+$redirect = new Redirector(new UrlGenerator($router->getRoutes(), $request));
+
+// use redirect
+// return $redirect->home();
+// return $redirect->back();
+// return $redirect->to('/');
 
 // Dispatch the request through the router
 $response = $router->dispatch($request);
