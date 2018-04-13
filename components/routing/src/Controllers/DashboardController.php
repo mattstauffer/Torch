a7f2f58c8ba96637fb19957588282f530320bb05
Aken Roberts <aken@cryode.com>
2018-04-12 23:52:28 -0500

    Remove return typehints

--- components/routing/src/Controllers/DashboardController.php
+++ components/routing/src/Controllers/DashboardController.php
@@ -4,21 +4,21 @@ namespace Torch\Routing\Controllers;
 
 class DashboardController
 {
-    public function login(): string
+    public function login()
     {
         $_SESSION['logged_in'] = true;
 
         return 'Successfully logged in! <a href="/dashboard">View Dashboard</a>';
     }
 
-    public function logout(): string
+    public function logout()
     {
         unset($_SESSION['logged_in']);
 
         return 'Successfully logged out. <a href="/">Return to Home</a>';
     }
 
-    public function dashboard(): string
+    public function dashboard()
     {
         return 'This is the dashboard for logged-in users. <a href="/logout">Log Out</a>';
     }
