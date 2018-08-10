5eca878c511d40bf9404b6e4c8a296f45f87f834
Matt Loberg <loberg.matt@gmail.com>
2018-08-10 17:45:47 -0500

    update queue component for 5.5

--- components/queue/config/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'app' => [
-        'key' => 'eblQq5Xx6qm6wHqHVmH6fUt2iMlqxvUU', // Set this uniquely for your app! 32-char-long string for AES-256-CBC
-        'cipher' => 'AES-256-CBC'
-    ]
-];
