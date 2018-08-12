de6706cf1aabadedc8b2fa4fb4821d942d440a31
Ahmad Hussain <ahmadajzr@gmail.com>
2018-08-12 23:49:56 +0200

    CS fixes

--- components/pagination/templates/page.php
+++ components/pagination/templates/page.php
@@ -1,24 +1,24 @@
 <ul class="pagination">
     <!-- Previous Page Link -->
-    <?php if ($paginator->onFirstPage()): ?>
+    <?php if ($paginator->onFirstPage()) : ?>
         <li class="disabled"><span>&laquo;</span></li>
-    <?php else: ?>
+    <?php else : ?>
         <li><a href="<?php echo $paginator->previousPageUrl(); ?>" rel="prev">&laquo;</a></li>
     <?php endif; ?>
 
     <!-- Pagination Elements -->
-    <?php foreach ($elements as $element): ?>
+    <?php foreach ($elements as $element) : ?>
         <!-- "Three Dots" Separator -->
         <?php if (is_string($element)) : ?>
             <li class="disabled"><span><?php echo $element;?></span></li>
         <?php endif; ?>
 
         <!-- Array Of Links -->
-        <?php if (is_array($element)): ?>
-            <?php foreach ($element as $page => $url): ?>
-                <?php if ($page == $paginator->currentPage()): ?>
+        <?php if (is_array($element)) : ?>
+            <?php foreach ($element as $page => $url) : ?>
+                <?php if ($page == $paginator->currentPage()) : ?>
                     <li class="active"><span><?php echo $page; ?></span></li>
-                <?php else: ?>
+                <?php else : ?>
                     <li><a href="<?php echo $url; ?>"><?php echo $page; ?></a></li>
                 <?php endif; ?>
             <?php endforeach; ?>
@@ -26,9 +26,9 @@
     <?php endforeach; ?>
 
     <!-- Next Page Link -->
-    <?php if ($paginator->hasMorePages()): ?>
+    <?php if ($paginator->hasMorePages()) : ?>
         <li><a href="<?php echo $paginator->nextPageUrl(); ?>" rel="next">&raquo;</a></li>
-    <?php else: ?>
+    <?php else : ?>
         <li class="disabled"><span>&raquo;</span></li>
     <?php endif; ?>
 </ul>
