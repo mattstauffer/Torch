26a41c8588933b59311d27160fba0ee5a8b94017
Ahmad Hussain <ahmadajzr@gmail.com>
2018-08-13 22:24:54 +0200

    Add Global Middleware to middleware component

--- components/middleware/index.php
+++ components/middleware/index.php
@@ -7,21 +7,29 @@
  * @contributor https://github.com/dead23angel
  * @contributor Matt Stauffer
  */
-session_start();
 
 require_once 'vendor/autoload.php';
 
-use Illuminate\Container\Container;
-use Illuminate\Events\Dispatcher;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Router;
+use Illuminate\Routing\Pipeline;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+
+// Create new IoC Container instance
+$container = new Container;
 
 // Using Illuminate/Events/Dispatcher here (not required); any implementation of
 // Illuminate/Contracts/Event/Dispatcher is acceptable
-$events = new Dispatcher(new Container);
+$events = new Dispatcher($container);
 
 // Create the router instance
 $router = new Router($events);
+
+// Global middlewares
+$globalMiddleware = [
+    \App\Middleware\StartSession::class,
+];
 
 // Array middlewares
 $routeMiddleware = [
@@ -40,8 +48,22 @@ require_once 'routes.php';
 // Create a request from server variables
 $request = Request::capture();
 
-// Dispatch the request through the router
-$response = $router->dispatch($request);
+// Dispatching the request:
+// When it comes to dispatching the request, you have two options
+// a) you either send the request directrly through the router
+// or b) you pass the request object through a stack of (global) middlewares
+// then dispatch it.
+
+// a. Dispatch the request through the router
+// $response = $router->dispatch($request);
+
+// b. Pass the request through the global middlewares pipeline then dispatch it through the router
+$response = (new Pipeline($container))
+                    ->send($request)
+                    ->through($globalMiddleware)
+                    ->then(function ($request) use ($router) {
+                        return $router->dispatch($request);
+                    });
 
 // Send the response back to the browser
 $response->send();
