cba4af38b9b2c045702f5b42fd8b848be37210c2
Ahmad Hussain <ahmadajzr@gmail.com>
2018-08-15 23:55:57 +0200

    Add named routes to routing component

--- components/routing/routes.php
+++ components/routing/routes.php
@@ -13,9 +13,24 @@ $router->get('bye', function () {
 });
 
 $router->group(['namespace' => 'App\Controllers', 'prefix' => 'users'], function (Router $router) {
-    $router->get('/', ['name' => 'users.index', 'uses' => 'UsersController@index']);
-    $router->post('/', ['name' => 'users.store', 'uses' => 'UsersController@store']);
+    $router->get('/', ['as' => 'users.index', 'uses' => 'UsersController@index']);
+    $router->post('/', ['as' => 'users.store', 'uses' => 'UsersController@store']);
 });
+
+// Naming routes using `as`:
+$router->get('/articles', ['as' => 'articles.index', 'uses' => function () use ($urlGenerator) {
+    return 'The url to the route named "articles.index" is: ' . $urlGenerator->route('articles.index');
+}]);
+
+// Naming routes using `name` method:
+// Note: You are responsible for refreshing names & action lookups if you named your
+// routes using `name` method otherwise an exception of "route not defined" will be thrown.
+$router->get('/articles/create', function () use ($urlGenerator) {
+    return 'The url to the route named "articles.create" is: ' . $urlGenerator->route('articles.create');
+})->name('articles.create');
+
+$router->getRoutes()->refreshNameLookups();
+$router->getRoutes()->refreshActionLookups();
 
 // catch-all route
 $router->any('{any}', function () {
