2290b989d8648cf9ac9ccb6feb943d308126e634
Stéphane Bour <sbour@welcome-media.fr>
2020-05-23 23:49:04 +0200

    chore(routing): add route name example (home)

--- components/routing/routes.php
+++ components/routing/routes.php
@@ -1,10 +1,11 @@
 <?php
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Router;
 
 /** @var $router Router */
 
-$router->get('/', function () {
+$router->name('home')->get('/', function () {
     return 'hello world!';
 });
 
@@ -15,6 +16,11 @@ $router->get('bye', function () {
 $router->group(['namespace' => 'App\Controllers', 'prefix' => 'users'], function (Router $router) {
     $router->get('/', ['name' => 'users.index', 'uses' => 'UsersController@index']);
     $router->post('/', ['name' => 'users.store', 'uses' => 'UsersController@store']);
+});
+
+// Redirect
+$router->get('/menu', function () use ($router) {
+    return new RedirectResponse($router->getRoutes()->getByName('home')->uri());
 });
 
 // catch-all route
