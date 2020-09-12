7671f99918e0119d73b7fe9f1be3ed38042fb906
Nathan E <nathane@users.noreply.github.com>
2020-09-12 00:10:10 -0400

    chore: update builder params

--- other-components/html/index.php
+++ other-components/html/index.php
@@ -9,26 +9,43 @@
 
 require_once 'vendor/autoload.php';
 
-use Collective\Html\HtmlBuilder;
 use Collective\Html\FormBuilder;
+use Collective\Html\HtmlBuilder;
 use Illuminate\Config\Repository as Config;
 use Illuminate\Container\Container;
 use Illuminate\Events\Dispatcher;
-use Illuminate\Http\Request;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Router;
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Session\SessionManager;
+use Illuminate\View\Engines\EngineResolver;
+use Illuminate\View\Factory;
+use Illuminate\View\FileViewFinder;
+use Illuminate\View\View;
+
+$pathsToTemplates = [__DIR__ . '/templates'];
+$pathToCompiledTemplates = __DIR__ . '/compiled';
+
+// Dependencies
+$filesystem = new Filesystem;
+$eventDispatcher = new Dispatcher(new Container);
+
+// Create View Factory capable of rendering PHP and Blade templates
+$viewResolver = new EngineResolver;
+$viewFinder = new FileViewFinder($filesystem, $pathsToTemplates);
+$viewFactory = new Factory($viewResolver, $viewFinder, $eventDispatcher);
 
 $app           = new Container();
 $app['events'] = new Dispatcher();
+$app['files']  = new Filesystem;
+$app['view'] = $viewFactory;
 $app['config'] = new Config([
     'session' => [
         'lifetime' => 120,
         'expire_on_close' => false
     ]
 ]);
-$app['files']  = new Filesystem;
 
 $app['config']['session.lottery'] = [2, 100];
 $app['config']['session.cookie'] = 'laravel_session';
@@ -51,13 +68,13 @@ $request = Request::capture();
 $url = new UrlGenerator($router->getRoutes(), $request);
 
 // Create HtmlBuilder
-$html = new HtmlBuilder($url);
+$html = new HtmlBuilder($url, $app['view']);
 
 // Create FormBuilder
-$form = new FormBuilder($html, $url, $app['session.store']);
+$form = new FormBuilder($html, $url, $app['view'], $request);
 
 echo $form->open(['url' => 'foo/bar']);
-echo $form->select('animal',[
+echo $form->select('animal', [
     'Cats' => ['leopard' => 'Leopard'],
     'Dogs' => ['spaniel' => 'Spaniel'],
 ]);
