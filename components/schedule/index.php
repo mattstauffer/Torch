cd816607e0a267fc78fe4986a8a2f87863cb59d8
Tom Witkowski <dev.gummibeer@gmail.com>
2021-01-19 20:46:54 +0100

    #41: add schedule component

---
+++ components/schedule/index.php
@@ -0,0 +1,102 @@
+<?php
+
+use Illuminate\Filesystem\Filesystem;
+
+require_once 'vendor/autoload.php';
+date_default_timezone_set('UTC');
+
+// custom app class with required methods
+class App extends \Illuminate\Container\Container
+{
+    public function isDownForMaintenance()
+    {
+        return false;
+    }
+
+    public function environment(): string
+    {
+        return 'torch';
+    }
+}
+
+// initialize container/app
+$container = App::getInstance();
+$container->instance('app', $container);
+
+// initialize exception handler
+$container->bind(\Illuminate\Contracts\Debug\ExceptionHandler::class, function () {
+    return new class implements \Illuminate\Contracts\Debug\ExceptionHandler {
+        public function shouldReport(Throwable $e)
+        {
+            throw $e;
+        }
+
+        public function report(Throwable $e)
+        {
+            throw $e;
+        }
+
+        public function render($request, Throwable $e)
+        {
+            throw $e;
+        }
+
+        public function renderForConsole($output, Throwable $e)
+        {
+            throw $e;
+        }
+    };
+});
+
+// initialize event dispatcher
+$container->singleton(\Illuminate\Contracts\Events\Dispatcher::class, function (\Illuminate\Container\Container $container) {
+    return new \Illuminate\Events\Dispatcher($container);
+});
+
+// initialize file cache for schedule mutex
+$container->instance('config', new \Illuminate\Config\Repository(require __DIR__ . '/config.php'));
+$container->instance('files', new Filesystem);
+$container->singleton(\Illuminate\Contracts\Cache\Factory::class, function (\Illuminate\Container\Container $container) {
+    return new \Illuminate\Cache\CacheManager($container);
+});
+
+// initialize schedule
+$container->singleton(\Illuminate\Console\Scheduling\Schedule::class, function (): \Illuminate\Console\Scheduling\Schedule {
+    return tap(new \Illuminate\Console\Scheduling\Schedule('UTC'), function (\Illuminate\Console\Scheduling\Schedule $schedule): void {
+        $schedule->useCache('file'); // cache store name to use for mutex
+    });
+});
+
+// configure schedule with scheduled commands/callbacks like normal
+$schedule = $container->make(\Illuminate\Console\Scheduling\Schedule::class);
+
+$schedule->call(function () {
+    echo 'hello from schedule - always'.PHP_EOL;
+})->everyMinute()->name('torch:always');
+
+$schedule->call(function () {
+    echo 'hello from schedule - every five'.PHP_EOL;
+})->everyFiveMinutes()->name('torch:every5');
+
+$schedule->call(function () {
+    echo 'hello from schedule - without overlapping'.PHP_EOL;
+    sleep(90);
+})->everyMinute()->name('torch:without-overlapping')->withoutOverlapping();
+
+// initialize schedule:run command
+$scheduler = new \Illuminate\Console\Scheduling\ScheduleRunCommand();
+$scheduler->setLaravel($container);
+$input = new \Symfony\Component\Console\Input\ArrayInput([]);
+$output = new \Symfony\Component\Console\Output\BufferedOutput();
+$scheduler->setInput($input);
+$scheduler->setOutput(new \Illuminate\Console\OutputStyle($input, $output));
+
+// run command without console application around
+$scheduler->handle(
+    $schedule,
+    $container->make(\Illuminate\Contracts\Events\Dispatcher::class),
+    $container->make(\Illuminate\Contracts\Debug\ExceptionHandler::class)
+);
+
+// echo buffered command output
+echo PHP_EOL.$output->fetch();
